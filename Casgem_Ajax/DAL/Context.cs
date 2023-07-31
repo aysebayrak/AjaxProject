ce46e06d296e991b32c65e77d526911011a62e72
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-31 14:17:25 +0300

    Add project files.

---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-D19MH5E\\SQLEXPRESS;initial catalog=CasgemDbAjax;integrated security=true");
+        }
+
+       public    DbSet<Customer>  Customers { get; set; }
+
+
+    }
+}
